a245661798623d00e07bf5e933491b19e6779aac
Diego Aldo Burlando <diego@dmm888.com>
2016-12-01 20:59:58 +0000

    Added imports serverside for mongo collections

--- app/server/main.js
+++ app/server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+var MajorArcana = new Mongo.Collection("MajorArcana");
 
 Meteor.startup(() => {
+
+    console.log(MajorArcana.find().fetch())
+    
   // code to run on server at startup
 });
