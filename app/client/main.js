2933751daaf3b363818c37f8d9df6c37cace72b0
Diego Aldo Burlando <diego@dmm888.com>
2016-12-01 22:10:15 +0000

    Added d3 package and underscore package

--- app/client/main.js
+++ app/client/main.js
@@ -1,11 +1,21 @@
+/*global d3*/
+
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  
+  
+  var ar =[51,3,5,3,7,88,3];
+  console.log(d3.min(ar));
+  
+  
+  console.log(d3.range(0, 1, 0.016));
 });
 
 Template.hello.helpers({
