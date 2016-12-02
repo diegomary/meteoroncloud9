12ac2fe856b33c976ae6e1970d09c3f9a954fd8c
Diego Aldo Burlando <diego@dmm888.com>
2016-12-02 11:45:17 +0000

    Added http logic

--- app/client/main.js
+++ app/client/main.js
@@ -3,30 +3,63 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+
+  this.flowers = new ReactiveVar([]);
+  this.flowers1 = new ReactiveVar([]);
+  var tmpl=Template.instance();
   
   
-  var ar =[51,3,5,3,7,88,3];
-  console.log(d3.min(ar));
-  
-  
-  console.log(d3.range(0, 1, 0.016));
+  HTTP.call( 'GET', 'https://dmm888enhanced.apphb.com/api/apicode/GetFlowers', {}, function( error, response ) {
+  if ( error ) {
+   console.log( error );
+   return;
+  } 
+
+  tmpl.flowers1.set(response.data); });
+
 });
 
+
 Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+  
+
+  flowers () {
+
+   return Template.instance().flowers.get();
+
   },
+  
+  
+  flowers1 () {
+
+   return Template.instance().flowers1.get();
+
+  },
+  
+  
+  
+  
+  
+  
+  
 });
 
 Template.hello.events({
+  
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+   
+  HTTP.call( 'GET', 'https://dmm888enhanced.apphb.com/api/apicode/GetFlowers', {}, function( error, response ) {
+  if ( error ) {
+   console.log( error );
+   return;
+  } 
+
+  instance.flowers.set(response.data); });
+ 
   },
+  
+  
 });
