72e1b89aaf9d956fc07ac3c1aad37943f6d135cc
Diego Aldo Burlando <diego@dmm888.com>
2016-12-02 16:35:46 +0000

    Added script for local machin startup

--- app/run.sh
+++ app/run.sh
@@ -1,1 +1,3 @@
+meteor npm install --save babel-runtime
+
 MONGO_URL="mongodb://diegomary:atreius@ds061371.mlab.com:61371/diegomary88" meteor --port $IP:$PORT
---
+++ app/runlocal.sh
@@ -0,0 +1,2 @@
+meteor npm install --save babel-runtime
+MONGO_URL="mongodb://diegomary:atreius@ds061371.mlab.com:61371/diegomary88" meteor --port 3000
